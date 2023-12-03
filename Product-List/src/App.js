eb5a3d62319930e20baa99f5e6f7ad36fa1125e8
hakan <tasbeyhakan@gmail.com>
2023-12-03 23:17:46 +0300

    first commit

---
+++ Product-List/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header/Header";
+import Product from "./components/ProductCard/Product";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Product />
+    </div>
+  );
+}
+
+export default App;
---
+++ Product-List/src/components/ProductCard/Product.jsx
@@ -0,0 +1,20 @@
+import "./Product.scss";
+import { products } from "../../helper/data.js";
+
+const Product = () => {
+  return (
+    <div className="container">
+      {products.map((item, index) => (
+        <div key={index} className="card">
+          <div className="card-over">
+            <p>{item.title}</p>
+          </div>
+          <h1>{item.price} $</h1>
+          <img src={item.image} alt="" />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Product;
